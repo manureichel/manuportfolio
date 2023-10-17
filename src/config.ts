470973678016afcf11fba37bc2ea162db8bd889b
Manuel Reichel <manuelreichel@gmail.com>
2023-10-17 13:11:07 -0300

    added google token

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export const SITE_TITLE = "Manuel Reichel";
 export const SITE_DESCRIPTION =
   "A programming blog with content related to web development and DevOps, utilities, among others";
 export const MY_NAME = "Manuel Reichel";
+export const GOOGLE_TOKEN = "9-Jveu-N5eMMZOVzBUkTwb_bSSTz4iPAdmcouznqHN4"
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
